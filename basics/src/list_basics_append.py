09f76d36603035641480ca4ffc6125c81795842a
amar <amar40408085@gmail.com>
2026-07-31 09:29:13 +0530

    Basics - Mutable and Immutable

---
+++ basics/src/list_basics_append.py
@@ -0,0 +1,3 @@
+list_1 = [1, 2, 3]
+list_1.append(4)
+print(list_1) # Output: [1, 2, 3, 4]
---
+++ basics/src/string_basics_immutable.py
@@ -0,0 +1,3 @@
+s = "Python"
+s[0] = "J"       # TypeError: 'str' object does not support item assignment
+print(s)
